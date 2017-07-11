bce3f404f82800b519034681728cca3e323b1c22
300931676 <300931676@PR-A315-13>
2017-07-11 17:29:23 -0400

    Added human abstract class

---
+++ Comp123-Sum2017-Lesson9B/Human.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+* Name:Gursharan Singh
+* Date:July11,2017
+* Description: Human Abstract CLass
+* Version:0.1 Created the Abstract Human class
+*/
+
+namespace Comp123_Sum2017_Lesson9B
+{
+    /// <summary>
+    /// This is the abstract human class
+    /// </summary>
+
+    public abstract class Human
+    {
+        //PRIVATE INSTANCE VARIABLES
+
+        private string _name;
+
+        // Public Properties
+        public string Name { get
+            {
+                return this._name;
+            } set
+            {
+                this._name = value;
+            }
+        }
+        /// <summary>
+        /// This is the main constructor for the human class
+        /// It takes one parameter -name(string)
+        /// </summary>
+        /// <param name="name"></param>
+        //CONSTRUCTORS-----------------------------
+        public Human(string name)
+        {
+            this.Name = name;
+        }
+        //PRIVATE METHODS
+        //PUBLIC METHODS
+    }
+}
