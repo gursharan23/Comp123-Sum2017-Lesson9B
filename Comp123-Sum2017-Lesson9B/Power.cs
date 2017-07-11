e2b24c1326e5a31f10679890101f5e2820345f6b
300931676 <300931676@PR-A315-13>
2017-07-11 17:39:57 -0400

    Added power class

---
+++ Comp123-Sum2017-Lesson9B/Power.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+ * Name:Gursharan Singh
+ * Date:July11,2017
+ * Description: This is the power class
+ * Version:0.1 Created the power class
+ */
+namespace Comp123_Sum2017_Lesson9B
+{
+    /// <summary>
+    /// This is the power class which is used as a datatype
+    /// </summary>
+    public class Power
+    {
+        //Public properties
+        public string Name { get; set; }
+        public int Rank { get; set; }
+        //Constructors
+    }
+}
