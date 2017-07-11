6026f01c1cf497ce22a7c466668e9bea58f3008a
300931676 <300931676@PR-A315-13>
2017-07-11 17:35:45 -0400

    Added super human class

---
+++ Comp123-Sum2017-Lesson9B/SuperHuman.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+ * Name:Gursharan Singh
+ * Date:July11,2017
+ * Description: This is the super human class extends the human abstract class
+ * Version:0.1 Created the super human class
+ */
+namespace Comp123_Sum2017_Lesson9B
+{
+    /// <summary>
+    /// This is the super human class
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        //Private instance variables
+
+        //Public properties
+        //Constructors
+        public SuperHuman(string name):base(name)
+        {
+        }
+        //Private methods
+        //Public methods
+    }
+}
