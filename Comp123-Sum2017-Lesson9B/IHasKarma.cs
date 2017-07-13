e433e352ee1995a4f904a10179dadb249961ef3d
300931676 <300931676@PR-A315-13>
2017-07-13 13:22:16 -0400

    Added IHasKarma Interface

---
+++ Comp123-Sum2017-Lesson9B/IHasKarma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name:Gursharan Singh
+ * Date:July13,2017
+ * Description: This is an interface that defines the Karma Property that
+ * must be implemented in every class that subscribes to it.
+ * Version:0.1 Created IHasKarma Interface
+ */
+namespace Comp123_Sum2017_Lesson9B
+{
+    /// <summary>
+    /// This is the IHasKarma Interface
+    /// </summary>
+    public interface IHasKarma
+    {
+       
+        //Public properties
+        int Karma { get; set; }
+    }
+}
